1e60f870edf4f2f1509a443fc8981e82a4feb422
Grisha <grisha1999.15@gmail.com>
2019-08-26 16:46:25 +0400

    First commit

---
+++ mod_csv.py
@@ -0,0 +1,104 @@
+#!/usr/bin/python
+
+import sys, getopt
+import csv
+
+def open_csv_file(filename):
+    return open(filename, "rb")
+
+def close_csv_file(fd):
+    fd.close()
+
+def write_csv_file(filename, data):
+    fd = open(filename, "w")
+    writer = csv.writer(fd)
+    writer.writerows(data)
+    fd.close()
+
+def read_data(fd):
+    data = csv.reader(fd)
+    data = strip_data(data)
+    return data
+
+def get_values(data, index_row):
+    for r in data:
+        yield r[index_row]
+
+def strip_data(data):
+    for r in data:
+        while r[-1] == '' and len(r) > 8:
+           r.pop(-1)
+        yield r
+
+def minute_to_second(values):
+    for val in values:
+        new_val = val.replace("m", "").replace("s", "")
+        if unicode(new_val).isnumeric():
+            new_val = str(int(new_val))
+        if len(new_val) > 2:
+            minute = int(new_val[:-2])
+            second = int(new_val[-2:])
+            yield str(minute*60 + second)
+        else:
+            yield new_val
+
+def set_opet_states(data, times, value):
+    for i in range(len(data) - 1):
+        if data[i+1][1] == "Open":
+            times[i] = value
+
+def set_notequiv_state(data, times, value):
+    for i in range(len(data) - 1):
+        if data[i+1][1] == "Not Equiv":
+            times[i] = value
+
+def get_mults_size(names):
+    A = []
+    B = []
+    for i in range(len(names)):
+        i1 = names[i].rfind('_')
+        i2 = names[i].rfind('x')
+        i3 = names[i].rfind('.')
+        A.append(names[i][i1+1:i2])
+        B.append(names[i][i2+1:i3])
+    return A, B
+
+def modify_data(data, new_col, modify = False, col_index = 0):
+    for i in range(len(data)):
+        if (modify):
+            data[i][col_index] = new_col[i]
+        else:
+            data[i].append(new_col[i])
+
+def main(input_file, output_file):
+
+    ifile = open_csv_file(input_file)
+    data = list(read_data(ifile))
+
+    times = get_values(data, 5)
+    names = get_values(data, 0)
+    next(times); next(names)
+    times = minute_to_second(times)
+
+    times = list(times)
+    names = list(names)
+
+    set_opet_states(data, times, -1)
+    set_notequiv_state(data, times, -2)
+
+    times.insert(0, "Time")
+
+    A, B = get_mults_size(names)
+    A.insert(0, 'A')
+    B.insert(0, 'B')
+    modify_data(data, times, True, 5)
+    modify_data(data, A)
+    modify_data(data, B)
+
+    if (output_file != ""):
+        write_csv_file(output_file, data)
+    
+    return data
+
+if __name__ == "__main__":
+    main("RESULT.csv", "RESULT_mod.csv")
---
+++ plot.py
@@ -0,0 +1,81 @@
+#!/usr/bin/python
+
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import matplotlib as mpl
+import seaborn as sns
+import sys, getopt
+
+import mod_csv
+
+def main():
+    input_file = "RESULT.csv"
+    output_file = "RESULT_mod.csv"
+    try:
+        opts, args = getopt.getopt(sys.argv[1:], "hi:o:", ["help", "input_file=", "output_file="])
+    except getopt.GetoptError:
+        print ('Invalid option')
+        print ('USAGE : plot.py -i <input file> -o <output file>')
+        sys.exit(2)
+
+    for opt, arg in opts:
+        if opt in ("-h", "--help"):
+            print ('plot.py -i <input file> -o <output file>')
+            sys.exit()
+        elif opt in ("-o", "--output_file"):
+            output_file = arg
+        elif opt in ("-i", "--input_file"):
+            input_file = arg
+
+    mod_csv.main(input_file, output_file)
+
+    flights_raw = pd.read_csv(output_file)
+    flight_matrix = flights_raw.pivot_table(values="Time", index="B", columns="A", dropna=False)
+
+    ind = range(1, 81)
+    col = range(1, 81)
+
+    df = pd.DataFrame(index=ind, columns=col)
+    df.index.name = 'B'
+    df.columns.name = 'A'
+
+    df.update(flight_matrix)
+
+    df.fillna(value=0, inplace=True)
+
+    yticks = df.index
+    keptticks = yticks[::int(len(yticks)/40)]
+    yticks = ['' for y in yticks]
+    yticks[::int(len(yticks)/40)] = keptticks
+
+    xticks = df.columns
+    keptticks = xticks[::int(len(xticks)/40)]
+    xticks = ['' for x in xticks]
+    xticks[::int(len(xticks)/40)] = keptticks
+
+    fig = plt.figure(figsize=(50,50))
+
+    colors = ["#66ff66","#00ff00", "#00b300", "#008000", "#004d00", "#ff6666", "#ff0000", "#b30000", "#800000"]
+    cmap = sns.blend_palette(colors, 100)
+    # cmap = sns.color_palette(colors, 100)
+
+    cmap1 = mpl.colors.ListedColormap(['w'])
+    cmap2 = mpl.colors.ListedColormap(['#0000ff'])
+    cmap3 = mpl.colors.ListedColormap(["Black"])
+
+    ticks = range(0,3601,100)
+    ticks.append(1)
+
+    cbar_kws = {"ticks":ticks}
+    r = sns.heatmap(df, linewidths=.5, cbar_kws=cbar_kws, cmap=cmap, yticklabels=yticks, xticklabels=xticks, vmin=1, vmax=3600, center=2500)
+    sns.heatmap(df, mask=(df != 0), cbar=False, linewidths=.5, cmap=cmap1, yticklabels=yticks, xticklabels=xticks, robust=True)
+    sns.heatmap(df, mask=(df != -1), linewidths=.5, cbar=False, cmap=cmap2, yticklabels=yticks, xticklabels=xticks, robust=True)
+    sns.heatmap(df, mask=(df != -2), linewidths=.5, cbar=False, cmap=cmap3, yticklabels=yticks, xticklabels=xticks, robust=True)
+    r.set_title("Original Heatmap")
+
+    plt.yticks(rotation=0)
+    plt.show()
+
+if __name__ == "__main__":
+    main()
